114834384eff0c545acc04faba028180427096a5
cutsea110 <cutsea110@gmail.com>
2017-02-09 13:29:01 +0900

    [DONE] 02 elm-arch/03-messages

---
+++ message/Main.elm
@@ -0,0 +1,47 @@
+module Main exposing (..)
+
+import Html exposing (Html, button, div, text, program)
+import Html.Events exposing (onClick)
+
+-- MODEL
+
+type alias Model = Bool
+
+init : (Model, Cmd Msg)
+init = (False, Cmd.none)
+
+-- MESSAGE
+
+type Msg = Expand | Collapse
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+    if model then
+        div [] [ button [onClick Collapse] [text "Collapse"]
+               , text "Widget"
+               ]
+    else
+        div [] [ button [onClick Expand] [text "Expand"]]
+
+-- UPDATE
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    case msg of
+        Expand -> (True, Cmd.none)
+        Collapse -> (False, Cmd.none)
+
+-- SUBSCRIPTION
+
+subs : Model -> Sub Msg
+subs model = Sub.none
+
+-- MAIN
+
+main = program { init = init
+               , view = view
+               , update = update
+               , subscriptions = subs
+               }
